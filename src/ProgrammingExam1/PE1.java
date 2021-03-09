3153bb46bb3885c77be4531afab0d422627836a4
visaagan <55763304+visaagan@users.noreply.github.com>
2021-03-09 01:11:40 -0500

    Update PE1.java

--- src/ProgrammingExam1/PE1.java
+++ src/ProgrammingExam1/PE1.java
@@ -3,26 +3,6 @@ import java.awt.Color;
 import java.awt.List;
 import java.util.ArrayList; 
 
-/* PLEASE DO NOT MODIFY A SINGLE STATEMENT IN THE TEXT BELOW.
-READ THE FOLLOWING CAREFULLY AND FILL IN THE GAPS
-
-I hereby declare that all the work that was required to 
-solve the following problem including designing the algorithms
-and writing the code below, is solely my own and that I received
-no help in creating this solution and I have not discussed my solution 
-with anybody. I affirm that I have read and understood
-the Senate Policy on Academic honesty at 
-https://secretariat-policies.info.yorku.ca/policies/academic-honesty-senate-policy-on/
-and I am well aware of the seriousness of the matter and the penalties that I will face as a 
-result of committing plagiarism in this assignment.
-
-BY FILLING THE GAPS,YOU ARE SIGNING THE ABOVE STATEMENTS.
-
-Full Name: Visaagan Sivagnanasuntharam
-Student Number: 216007817
-Course Section: EECS 2030 Section M
-*/
-
 public class PE1 {
 	public static void main(String[] args) {
 		
